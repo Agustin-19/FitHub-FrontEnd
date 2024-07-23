8aa1d7ee25243247381cc9652f3678904cb959d9
Agustin-19 <ottersttet@gmail.com>
2024-07-23 12:18:49 -0300

    estructura modificada  del proyecto, actualizacion fetchuser

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Dashboard() {
+    return (
+        <div className=" text-black bg-gray-100">
+            <h1>es entrenador o usuario?</h1>
+            <p> si entrenador llamamos al componente CoachDAshboard </p>
+            <p> si usuario llamamos al componente UserDAshboard </p>
+        </div>
+    );
+}
--- src/app/user/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function UserDashboard() {
-    return (
-        <div className=" text-black bg-gray-100">
-            <h1>Dashboard de usuario</h1>
-        </div>
-    );
-}
