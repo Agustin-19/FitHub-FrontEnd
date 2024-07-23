f44a946b73c40d9f29412dbc1235202850d7bb0f
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-23 19:55:35 -0300

    login/Register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { LoginComponet } from "../../components/Login";
 export default function Login() {
   return (
     <div className=" text-white bg-gray-100">
-      <h1>Login</h1>
+      <h1>Login </h1>
 
       <LoginComponet />
     </div>
