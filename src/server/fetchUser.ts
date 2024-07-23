80e80845c1c3b6e72c105bcff91dd6b393d53784
Agustin-19 <ottersttet@gmail.com>
2024-07-23 20:06:30 -0300

    fix

--- src/server/fetchUser.ts
+++ src/server/fetchUser.ts
@@ -1,8 +1,6 @@
 import { IUser, ILogin } from "@/interface/interface";
 const API = "http:api";
 
-const API = "http:api";
-
 export const postSigup = async (user: Omit<IUser, "id">) => {
   const response = await fetch(`${API}/users/register`, {
     method: "POST",
