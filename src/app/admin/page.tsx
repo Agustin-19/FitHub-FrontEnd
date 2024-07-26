ccf308c76f5a894770d1d955c5e2077670b76833
laura larrotta <larrotta.laura@gmail.com>
2024-07-25 19:26:13 -0500

    agregado filtro, proteccion

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 import CoachList from "../../components/CoachList/index";
 import { useContext } from "react";
 import { UserContext } from "@/context/userContext";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Admin() {
   const router = useRouter();
