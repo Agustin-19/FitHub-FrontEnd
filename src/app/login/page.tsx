75c68d965fd3b9982c5e5f1aadd3e3208559c685
laura larrotta <larrotta.laura@gmail.com>
2024-07-25 19:54:13 -0500

    protecciones, navegador y filtros

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
 import { LoginComponet } from "../../components/Login";
 import HumanScan from "../../components/HumanScan";
-
+import { UserContext } from "@/context/userContext";
+import { useRouter } from "next/navigation";
+import { useContext } from "react";
 export default function Login() {
+  const router = useRouter();
+  const { isLogged } = useContext(UserContext);
+
+  if (isLogged) {
+    router.push("/login");
+  }
   return (
     <div className="flex ">
       <div className="flex-1 flex justify-center items-center">
