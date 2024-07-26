1358245652dd1aa45bfcc143f94054056fb06103
laura larrotta <larrotta.laura@gmail.com>
2024-07-25 19:39:18 -0500

    Descripción de los cambios realizados en page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import HomeUser from "@/components/Home";
 
+import { useContext } from "react";
+import { UserContext } from "@/context/userContext";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
+  const { isLogged } = useContext(UserContext);
+
+  if (!isLogged) {
+    router.push("/login");
+  }
   return (
     <div>
       <HomeUser />
