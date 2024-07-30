1a362957498a57053c515c240c50910ecdea1f2a
laura larrotta <larrotta.laura@gmail.com>
2024-07-30 12:20:02 -0500

    login con nuevo diseño

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,9 @@
 import { LoginComponet } from "../../components/Login";
-import HumanScan from "../../components/HumanScan";
 
 export default function Login() {
   return (
-    <div className="flex ">
-      <div className="flex-1 flex justify-center items-center">
-        <HumanScan />
-      </div>
-      <div className="flex-1 flex justify-center items-center">
-        <LoginComponet />
-      </div>
+    <div className="flex-1 flex justify-center items-center">
+      <LoginComponet />
     </div>
   );
 }
