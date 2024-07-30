688f3a035218fbd4fad8476679dcad7974f0ada0
Guastalli Alexis Dante <165845724+ADGuastalli@users.noreply.github.com>
2024-07-30 19:24:17 -0300

    inicioMercadoPago

--- src/app/routines/page.tsx
+++ src/app/routines/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import { rutinas } from "../../../public/data/rutines.data";
 
---
+++ src/components/MercadoPago/indexSuccess.tsx
@@ -0,0 +1,7 @@
+export function MercadoPagoSuccess() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Tu pago se ha realizado con éxito</h1>
+    </div>
+  );
+}
